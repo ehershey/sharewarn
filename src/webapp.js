a0d6557b1868319223bfc571e486c0683b8cfced
Ernie Hershey <ernie.hershey@10gen.com>
2014-02-09 14:03:38 -0500

    initial web app

---
+++ src/webapp.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var os = require('os');
+var app = express();
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/', function(req, res) {
+  res.send("<html><body><a href='/hello.txt'>/hello.txt</a></body></html>");
+});
+
+var listen_port = 3000;
+app.listen(listen_port);
+console.log('Listening on port ' + listen_port);
+console.log('http://' + os.hostname() + ':' + listen_port + '/');
